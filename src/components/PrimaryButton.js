c9fd9715504aacb51c566c4860ca15ecd43c694e
noway <dragonfury1928@gmail.com>
2024-09-28 17:16:55 -0400

    Draggable destination marker on toggle and

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import '../App.css';
 
-export const PrimaryButton = ({text, onClick}) => {
+export const PrimaryButton = ({text, onClick, fontSize, padding}) => {
 	return (
 		<div style={{padding: 16, margin: 5}}>
 			<button
 				style={{
-					padding: '12px 24px',
+					padding: padding || '12px 24px',
 					backgroundColor: 'var(--primary-color)',
 					color: '#304b30',
 					border: 'none',
 					borderRadius: '5px',
-					fontSize: '1.5rem',
 					cursor: 'pointer',
 					transition: 'background-color 0.2s ease',
 					boxShadow: '0 4px 8px rgba(0, 0.1, 0.1, 0.2)',
-					fontFamily: 'Jua, sans-serif'
+					fontFamily: 'Jua, sans-serif',
+					fontSize: fontSize || '2vh'
 				}}
 				onMouseOver={(e) => (e.target.style.backgroundColor = 'var(--tertiary-color)')}
 				onMouseOut={(e) => (e.target.style.backgroundColor = 'var(--primary-color)')}
--- src/pages/PartyPages/HostPage.js
+++ src/pages/PartyPages/HostPage.js
@@ -1,43 +1,52 @@
 import React, {useState} from 'react';
-import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import {MapContainer, Marker, Popup, TileLayer, Tooltip} from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../../App.css';
-import {useLocation, useNavigate} from 'react-router-dom';
-import {PrimaryButton} from "../../components/PrimaryButton.js";
-
-
-const customIcon = L.icon({
-	iconUrl: 'https://cdn-icons-png.flaticon.com/512/609/609803.png',
-	iconSize: [38, 38],
-	iconAnchor: [22, 38],
-	popupAnchor: [-3, -36]
-});
-
-const sourceIcon = L.icon({
-	iconUrl: 'https://cdn-icons-png.freepik.com/512/5723/5723249.png',
-	iconSize: [48, 48],
-	iconAnchor: [22, 38],
-	popupAnchor: [-3, -36]
-});
+import {useLocation} from 'react-router-dom';
+import {PrimaryButton} from '../../components/PrimaryButton.js';
 
 const destinationIcon = L.icon({
 	iconUrl: 'https://cdn1.iconfinder.com/data/icons/color-bold-style/21/14_2-512.png',
 	iconSize: [38, 38],
 	iconAnchor: [22, 38],
-	popupAnchor: [-3, -36]
+	popupAnchor: [-3, -36],
 });
 
 const RoutePage = () => {
-	const navigate = useNavigate();
 	const location = useLocation();
 	const {host, partyName, date, time, description, partyCode} = location.state || {};
 	const [tooltipVisible, setTooltipVisible] = useState(false);
+	const [destinationMarkers, setDestinationMarkers] = useState([
+		{
+			id: 0,
+			position: [51.515, -0.1],
+			name: 'Destination Marker 0',
+			draggable: false,
+		},
+	]);
 
 	const handleCopyClick = () => {
 		navigator.clipboard.writeText(partyCode);
 		setTooltipVisible(true);
 		setTimeout(() => setTooltipVisible(false), 2000);
+	};
+
+	const handleDragEnd = (id, event) => {
+		const newPosition = event.target.getLatLng();
+		setDestinationMarkers((markers) =>
+			markers.map((marker) =>
+				marker.id === id ? {...marker, position: [newPosition.lat, newPosition.lng]} : marker
+			)
+		);
+	};
+
+	const toggleDraggable = (id) => {
+		setDestinationMarkers((markers) =>
+			markers.map((marker) =>
+				marker.id === id ? {...marker, draggable: !marker.draggable} : marker
+			)
+		);
 	};
 
 	return (
@@ -51,74 +60,98 @@ const RoutePage = () => {
 			}}
 		>
 			<div style={{width: '45%', padding: '20px', textAlign: 'center', color: 'white'}}>
-				<div style={{backgroundColor: 'var(--accent-color)', color: "var(--text-color)", padding: '10px', borderRadius: '15px'}}>
-					<div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+				<div
+					style={{
+						backgroundColor: 'var(--accent-color)',
+						color: 'var(--text-color)',
+						padding: '10px',
+						borderRadius: '15px',
+					}}
+				>
+					<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
 						<h2 style={{marginRight: '10px'}}>Welcome to your party!</h2>
-						<PrimaryButton text={`Your code is ${partyCode}`} onClick={handleCopyClick}/>
+						<PrimaryButton text={`Your code is ${partyCode}`} onClick={handleCopyClick} padding="8px 16px" fontSize="1.5vh"/>
 					</div>
-					<div style={{display: 'flex', justifyContent: 'center', gap: '10px'}}>
-						<div style={{position: 'relative'}}>
-
-							{tooltipVisible && (
-								<div
-									style={{
-										position: 'absolute',
-										top: '-15px',
-										left: '50%',
-										transform: 'translateX(-50%)',
-										backgroundColor: 'black',
-										color: 'white',
-										padding: '5px 10px',
-										borderRadius: '4px',
-										fontSize: '12px',
-										opacity: tooltipVisible ? 1 : 0,
-										visibility: tooltipVisible ? 'visible' : 'hidden'
-									}}
-								>
-									Added to clipboard
-								</div>
-							)}
-						</div>
+					<div style={{position: 'relative', marginTop: '10px'}}>
+						{tooltipVisible && (
+							<div
+								style={{
+									position: 'absolute',
+									top: '-15px',
+									left: '50%',
+									transform: 'translateX(-50%)',
+									backgroundColor: 'black',
+									color: 'white',
+									padding: '5px 10px',
+									borderRadius: '4px',
+									fontSize: '12px',
+								}}
+							>
+								Added to clipboard
+							</div>
+						)}
 					</div>
-					<div
-						style={{
-							display: 'grid',
-							gridTemplateColumns: '1fr 1fr',
-							gap: '12px',
-						}}
-					>
-						<div style={{textAlign: 'left', fontSize: '1.25rem', color: "var(--text-color)",}}>
-							<p>Host name: {host}</p>
-							<p>Party name: {partyName}</p>
-							<p>Date: {date}</p>
-							<p>Time: {time}</p>
-							<p>Description: {description}</p>
-						</div>
+					<div style={{textAlign: 'left', fontSize: '1.25rem', color: 'var(--text-color)', marginTop: '20px'}}>
+						<p>Host name: {host}</p>
+						<p>Party name: {partyName}</p>
+						<p>Date: {date}</p>
+						<p>Time: {time}</p>
+						<p>Description: {description}</p>
+						{destinationMarkers.map((marker) => (
+							<PrimaryButton text={marker.draggable ? 'Disable Drag Destination' : 'Enable Drag Destination'} key={marker.id}
+							               fontSize="2vh"
+							               padding="8px 16px"
+							               onClick={() => toggleDraggable(marker.id)}/>
+						))}
 					</div>
 
 				</div>
 
+				<MapContainer
+					center={[51.505, -0.09]}
+					zoom={13}
+					style={{
+						height: '50vh',
+						width: '100%',
+						marginTop: '20px',
+						borderRadius: '15px',
+						border: '1px solid tan',
+					}}
+				>
+					<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+					{destinationMarkers.map((marker) => (
+						<Marker
+							key={marker.id}
+							position={marker.position}
+							draggable={marker.draggable}
+							icon={destinationIcon}
+							eventHandlers={{
+								dragend: (event) => handleDragEnd(marker.id, event),
+							}}
+						>
+							{marker.draggable && (
+								<Tooltip permanent>
+									<span>Drag me!</span>
+								</Tooltip>
+							)}
 
-				<MapContainer center={[51.505, -0.09]} zoom={13}
-				              style={{height: '55vh', width: '100%', marginTop: '20px', borderRadius: 15, border: "tan"}}>
-					<TileLayer
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					/>
-					<Marker position={[51.505, -0.09]} icon={sourceIcon}>
-						<Popup>
-							Source
-						</Popup>
-					</Marker>
-					<Marker position={[51.505, -0.085]} icon={destinationIcon}>
-						<Popup>
-							Destination
-						</Popup>
-					</Marker>
-
+							<Popup>
+								Destination Marker {marker.id}: {marker.position[0].toFixed(4)}, {marker.position[1].toFixed(4)}
+							</Popup>
+						</Marker>
+					))}
 				</MapContainer>
 			</div>
 
-			<div style={{width: '45%', padding: '20px', textAlign: 'center', backgroundColor: 'var(--accent-color)', color: 'white'}}>
+			<div
+				style={{
+					width: '45%',
+					padding: '20px',
+					textAlign: 'center',
+					backgroundColor: 'var(--accent-color)',
+					color: 'white',
+				}}
+			>
 				<h2>Right Side Content</h2>
 				<p>This is the content on the right side.</p>
 			</div>
